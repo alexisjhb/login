7c434b7f7e0d3d484ebedb5d074b2d26f6dbe2c3
Alexis Hernández bolivar <169068450+alexisjhb@users.noreply.github.com>
2024-09-02 18:56:37 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+System.out.println("login");
+String login = sc.next();  
+ 
+if ("login == admin") {
+System.out.println("coloque sua senha: ");
+int senha = sc.nextInt(); 
+if(senha == senhaAdmin(in)){
+System.out.println("Acesso permitido");  
+} else(
+System.out.println("acesso negado!");
+
+  
+)
+}
+  
+  }
+}
